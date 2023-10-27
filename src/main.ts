0a3ad24a7d6bf1d74010ec6a8e4cbe266df08d9f
Guille <g.mirandagaleote@gmail.com>
2023-10-27 09:37:33 +0200

    Fix main to see itt in vercel

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 /* eslint-disable no-new */
 import './styles.css';
-import {Header} from './components/header';
 import {Main} from './components/mainc';
+import {Header} from './components/header';
 import {Pending} from './components/pending';
 
 function main() {
 	new Header('.container');
 	new Main('.container');
-	new Pending('.series');
+	new Pending('.container');
 }
 
 main();
